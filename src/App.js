d1615029e575beef7c381f77c747be5c7639fd0c
Tran Thang <tranthang0369@gmail.com>
2024-03-04 16:11:10 +0700

    install sass and font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Button from '~/componment/Button/index.js';
+
 function App() {
     return (
         <div className="App">
-            <Button />
-            <h1>123</h1>
+            <h1>React app</h1>
         </div>
     );
 }
