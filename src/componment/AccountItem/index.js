bf37140ddb8277affa3cb8795b1bad9cc5d66829
Tran Thang <tranthang0369@gmail.com>
2024-03-06 14:42:53 +0700

    Build Header #3

---
+++ src/componment/AccountItem/index.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/4b85df94ddbc913a995d4d721e417580.jpeg?lk3s=a5d48078&x-expires=1709820000&x-signature=CgGHTFBeADNEOBQuFMbyNCx1tvY%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>hoaa.hanassii</span> <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>Đào Lê Phương Hoa</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- src/componment/Layout/components/Header/index.js
+++ src/componment/Layout/components/Header/index.js
@@ -1,10 +1,23 @@
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import style from './Header.module.scss';
+import { useEffect, useState } from 'react';
+import { Wrapper as PopperWrapper } from '../../../Popper';
+import styles from './Header.module.scss';
+import AccountItem from '~/componment/AccountItem';
 
-const cx = classNames.bind(style);
+const cx = classNames.bind(styles);
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setInterval(() => {
+            setSearchResult([]);
+        }, 0);
+    });
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -44,16 +57,30 @@ function Header() {
                         ></path>
                     </svg>
                 </div>
-                <div className={cx('seach')}>
-                    <input placeholder="Seach accounts and videos" spellCheck={false} />
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
-                    <button className={cx('seach-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
+                <Tippy
+                    interactive={true}
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('seach')}>
+                        <input placeholder="Seach accounts and videos" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+
+                        <button className={cx('seach-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
                 <div className={cx('actions')}>Action</div>
             </div>
         </header>
